5b687edc4d42a94d79561496799df58e23e2064b
Turgay Demir <tdmr@Turgays-MacBook-Pro-2.local>
2021-12-07 11:49:58 +0100

    String

---
+++ string/string.js
@@ -0,0 +1,35 @@
+// String Methods
+// Index degerleri herzaman 0 dan baslar.
+// Istedigimiz bir degere baktigimiz da verdigimiz method calismassa degeri -1 olarak dondurur.
+
+// x.length => Verilen textin uzunlugunu bize verir.
+
+// x.slice(value1, value2) => Bu method da ise value1 dan baslayip value2-1 e kadar ki degeri bize verir.
+// x.slice(value) => Bu methodda usteki gibidir ama soyle bir durum var verilen degerden itibaren alir son value kadar. 
+// Tabiki negatif value degeri verilebilinir. 
+
+// x.substr() => Bu method slice() methodu ile ayni ama arasinda ki tek fark ise slice() methodu negatif deger alabilir ama substr() alamaz.
+
+// x.replace(value1, value2) => replace methodu ile bir deger yerine baska deger atamak isini yapar. value1 olan deger value2 atanan deger.
+
+// x.concat() => concat demek cancationation analamina gelir yani iki degeri birbirine birlestirir veya degerleri birbirine ekler.
+
+// x.indexOf() => indexOf anlami verilen bir harfin hanfi index numarasinda oldugunu gosterir.
+
+// x.trim() => Trim methodu string baslangic sonun daki bosluklari siler ve temizler.
+
+// x.charAt(value) => Verilen index numarasinin hangi harf oldugunu gosterir.
+
+// x.charCodeAt(value) => charCodeAt verilen valuenun unicode karakter numarasinin dondurur.
+
+// x.split() => Degerlerin birbirinin arasinda ki ayraclari gosterir. split(","), split("/") vb.
+
+// -slice(start, end)
+// -sunstring(start, end)
+// -substr(start, length)
+
+
+
+
+
+
