6348e4076c9c492ff523c9c12bac84d44f1acf51
Turgay Demir <89524827+TurgayD34@users.noreply.github.com>
2022-01-02 05:51:41 +0100

    Hoisting

---
+++ hoisting/hoisting.js
@@ -0,0 +1,9 @@
+x = 7;
+console.log(x);
+var x;
+// Bu kodun calismasinda sorun olmaz
+
+x = 7;
+console.log(x + ": " + y);
+var x, y = 10;
+// Ama bu kodun calismasinda sorun olacak cunku deger tanimlanmasi yazdirildiktan sonra oldugu icin.
