28b0eba2dbc2cfd8770b7d13f18bbedd82446baf
Turgay Demir <89524827+TurgayD34@users.noreply.github.com>
2022-01-02 05:52:03 +0100

    Use Strict

---
+++ use-strict/strict.js
@@ -0,0 +1,9 @@
+// x = 7;
+// document.getElementById('result').innerHTML = x;
+// Simdi x bastigimiz da ekranda okuyabilirsiniz.
+
+"use strict";
+x = 7;
+document.getElementById('result').innerHTML = x;
+// Ama simdi ise x is not defined hatasi alacagiz.
+
